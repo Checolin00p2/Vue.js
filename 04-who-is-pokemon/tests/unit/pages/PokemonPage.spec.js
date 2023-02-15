9e31b73aec6466a468daba1a276d6b645320e8bb
Sergio Garcia <sgarcia44@ucol.mx>
2023-02-15 09:13:35 -0600

    TEST - Fin de la seccion 8

--- 04-who-is-pokemon/tests/unit/pages/PokemonPage.spec.js
+++ 04-who-is-pokemon/tests/unit/pages/PokemonPage.spec.js
@@ -38,4 +38,55 @@ describe('PokemonPage Component',()=>{
 
         expect(wrapper.html()).toMatchSnapshot()
     })
+
+    test('Debe de mostrar los componentes de PokemonPicture y PokemonOptions',()=>{
+        const wrapper = shallowMount(PokemonPage,{
+            data(){
+                return{
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: "",
+                }
+            }
+        })
+
+        const picture = wrapper.find('pokemon-picture-stub')
+        const options = wrapper.find('pokemon-options-stub')
+
+        expect(picture.exists()).toBeTruthy()
+        expect(options.exists()).toBeTruthy()
+
+
+        expect(picture.attributes('pokemonid')).toBe('1')
+        expect(options.attributes('pokemons')).toBeTruthy()
+        
+        
+
+    })
+
+    test('Prueba de checkAnswer',async()=>{
+        const wrapper = shallowMount(PokemonPage,{
+            data(){
+                return{
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: "",
+                }
+            }
+        })
+
+        await wrapper.vm.checkAnswer(1)
+        expect(wrapper.find('h2').exists()).toBeTruthy()
+        expect(await wrapper.vm.showPokemon).toBe(true)
+        expect(wrapper.find('h2').text()).toBe(`Correcto, ${pokemons[0].name}`)
+
+        await wrapper.vm.checkAnswer(5)
+        expect(await wrapper.vm.message).toBe(`Oops, era ${pokemons[0].name}`)
+
+    })
+
 })
