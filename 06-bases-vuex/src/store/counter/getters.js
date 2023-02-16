cbc3108ca256e283d910ae8a9ea7230a89845644
Sergio Garcia <sgarcia44@ucol.mx>
2023-02-16 11:48:32 -0600

    Fin de seccion 10

---
+++ 06-bases-vuex/src/store/counter/getters.js
@@ -0,0 +1,5 @@
+
+
+export const squeareCount= (state) =>{
+    return state.count * state.count
+}
---
+++ 06-bases-vuex/src/store/counter/state.js
@@ -0,0 +1,6 @@
+export default ()=>({
+    count: 1,
+    lastMutation: 1,
+    isLoading:false,
+    lastRandomInt: 0
+})
